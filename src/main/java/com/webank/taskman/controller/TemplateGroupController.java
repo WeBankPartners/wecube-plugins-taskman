d15d4de65d0f23c6623a0669f70ef758402c58be
Jack <2651040479@qq.com>
2020-11-27 09:49:06 +0800

    #4 Add swagger annotation

--- src/main/java/com/webank/taskman/controller/TemplateGroupController.java
+++ src/main/java/com/webank/taskman/controller/TemplateGroupController.java
@@ -5,28 +5,28 @@ package com.webank.taskman.controller;
 import com.webank.taskman.dto.JsonResponse;
 import com.webank.taskman.dto.TemplateGroupVO;
 import com.webank.taskman.service.TemplateGroupService;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/template-group")
+@Api(tags = {"TemplateGroup接口使用"})
 public class TemplateGroupController {
     @Autowired
     TemplateGroupService templateGroupService;
 
     @PostMapping("/save")
+    @ApiOperation(value = "增加模板组",notes = "需要传入templateGroupVO对象")
     public JsonResponse createTemplateGroup(
             @RequestBody TemplateGroupVO templateGroupVO) throws Exception {
         templateGroupService.createTemplateGroupService(templateGroupVO);
         return JsonResponse.okay();
     }
 
-    @ApiImplicitParams({
-            @ApiImplicitParam(name = "templateGroupVO",value ="创建模板请求对象",readOnly = true )
-    })
-
     @PostMapping("edit")
+    @ApiOperation(value = "修改模板组",notes = "需要传入templateGroupVO对象")
     public JsonResponse updateTemplateGroup(
             @RequestBody TemplateGroupVO templateGroupVO) throws Exception {
         templateGroupService.updateTemplateGroupService(templateGroupVO);
@@ -34,11 +34,13 @@ public class TemplateGroupController {
     }
 
     @GetMapping("/selectAll")
+    @ApiOperation(value = "获取模板组数据",notes = "无需传入值")
     public JsonResponse selectAllTemplateGroup() throws Exception {
         return JsonResponse.okayWithData(templateGroupService.selectAllTemplateGroupService());
     }
 
     @GetMapping("/delete/{id}")
+    @ApiOperation(value = "删除模板组",notes = "需要传入id")
     public JsonResponse deleteTemplateGroupByID(@PathVariable("id") String id) throws Exception {
         templateGroupService.deleteTemplateGroupByIDService(id);
         return JsonResponse.okay();
--- src/main/java/com/webank/taskman/service/RequestTemplateService.java
+++ src/main/java/com/webank/taskman/service/RequestTemplateService.java
@@ -2,7 +2,10 @@ package com.webank.taskman.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.webank.taskman.domain.RequestTemplate;
+import com.webank.taskman.dto.RequestTemplateDTO;
+import com.webank.taskman.dto.RequestTemplateVO;
 
+import java.util.List;
 /**
  * <p>
  * 请求模板信息表  服务类
@@ -12,5 +15,13 @@ import com.webank.taskman.domain.RequestTemplate;
  * @since 2020-11-26
  */
 public interface RequestTemplateService extends IService<RequestTemplate> {
+
+    void AddRequestTemplate(RequestTemplateVO requestTemplateVO) throws Exception;
+
+    void updateRequestTemplate(RequestTemplateVO requestTemplateVO) throws Exception;
+
+    void deleteRequestTemplate(RequestTemplateVO requestTemplateVO) throws Exception;
+
+    List<RequestTemplateDTO> selectRequestTemplate(RequestTemplateVO requestTemplateVO) throws Exception;
 
 }

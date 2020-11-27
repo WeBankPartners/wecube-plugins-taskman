40f8e52da69f6c88e0add393e656e2a6862162dc
wanwei110 <1139777491@qq.com>
2020-11-27 09:34:28 +0800

    #4 Modify the VO class

--- src/main/java/com/webank/taskman/controller/TemplateGroupController.java
+++ src/main/java/com/webank/taskman/controller/TemplateGroupController.java
@@ -5,9 +5,10 @@ package com.webank.taskman.controller;
 import com.webank.taskman.dto.JsonResponse;
 import com.webank.taskman.dto.TemplateGroupVO;
 import com.webank.taskman.service.TemplateGroupService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 @RequestMapping("/template-group")
 public class TemplateGroupController {
@@ -20,6 +21,10 @@ public class TemplateGroupController {
         templateGroupService.createTemplateGroupService(templateGroupVO);
         return JsonResponse.okay();
     }
+
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "templateGroupVO",value ="创建模板请求对象",readOnly = true )
+    })
 
     @PostMapping("edit")
     public JsonResponse updateTemplateGroup(
--- src/test/java/com/webank/taskman/TemplateGroupTest.java
+++ src/test/java/com/webank/taskman/TemplateGroupTest.java
@@ -1,5 +1,6 @@
 package com.webank.taskman;
 
+import com.webank.taskman.dto.TemplateGroupDTO;
 import com.webank.taskman.dto.TemplateGroupVO;
 import com.webank.taskman.service.TemplateGroupService;
 import org.junit.Test;
@@ -7,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 
 @SpringBootTest
@@ -17,9 +20,18 @@ public class TemplateGroupTest {
 
     @Test
     public void testV1Group() throws Exception {
+//        TemplateGroupVO vo=new TemplateGroupVO();
+//        vo.setCreatedBy("11");
+//        vo.setUpdatedBy("22");
+//        vo.setManageRole("11");
+//        vo.setName("11");
+//        vo.setVersion("11");
+//        templateGroupService.createTemplateGroupService(vo);
         TemplateGroupVO vo=new TemplateGroupVO();
-        vo.setCreatedBy("11");
-        vo.setUpdatedBy("22");
-        templateGroupService.createTemplateGroupService(vo);
+        vo.setId("1332131518222503937");
+        vo.setCreatedBy("问我");
+        vo.setName("大答");
+
+        templateGroupService.updateTemplateGroupService(vo);
     }
 }

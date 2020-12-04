e99c35a8928748b4bcc7fa7be1adfedcf0e87973
wanwei110 <1139777491@qq.com>
2020-12-04 16:50:17 +0800

    #4 Task template

--- src/main/java/com/webank/taskman/controller/x100/TaskTemplateController.java
+++ src/main/java/com/webank/taskman/controller/x100/TaskTemplateController.java
@@ -7,27 +7,28 @@ import com.webank.taskman.domain.TaskTemplate;
 import com.webank.taskman.dto.*;
 import com.webank.taskman.dto.req.SaveRequestTemplateReq;
 import com.webank.taskman.dto.req.SaveTaskTemplateReq;
+import com.webank.taskman.dto.req.SelectTaskTemplateRep;
 import com.webank.taskman.dto.resp.RequestTemplateResp;
+import com.webank.taskman.dto.resp.TaskTemplateResp;
 import com.webank.taskman.dto.resp.TaskTemplateSVResp;
 import com.webank.taskman.service.RequestTemplateService;
 import com.webank.taskman.service.TaskTemplateService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @RestController
 @RequestMapping("/v1/task/template")
 @Api(tags = {"5、  TaskTemplate inteface API"})
 public class TaskTemplateController {
-
-    @Autowired
-    private RequestTemplateService requestTemplateService;
 
     @Autowired
     private TaskTemplateService taskTemplateService;
@@ -49,33 +50,36 @@ public class TaskTemplateController {
 
     @DeleteMapping("/delete/{id}")
     @ApiOperation(value = "delete TaskTemplate", notes = "需要传入id")
-    public JsonResponse deleteRequestTemplateByID(@PathVariable("id") String id) throws Exception {
-        requestTemplateService.deleteRequestTemplateService(id);
+    public JsonResponse deleteTaskTemplateByID(@PathVariable("id") String id) throws Exception {
+        taskTemplateService.deleteTaskTemplateByIDService(id);
         return JsonResponse.okay();
     }
 
-    @PostMapping("/search/{current}/{limit}")
-    @ApiOperation(value = "search TaskTemplate ")
-    @DynamicParameters(name = "req", properties = {
-            @DynamicParameter(name = "page", value = "页码", example = "", required = true, dataTypeClass = Integer.class),
-            @DynamicParameter(name = "pageSize", value = "每页行数", example = "100", required = true, dataTypeClass = Integer.class),
-            @DynamicParameter(name = "id", value = "主键", example = "", dataTypeClass = String.class),
-            @DynamicParameter(name = "name", value = "模板名称"),
-    })
-    public JsonResponse<QueryResponse<RequestTemplateResp>> selectRequestTemplate(
-            @PathVariable("current") Integer current,
-            @PathVariable("limit") Integer limit,
-            @RequestBody(required = false) SaveRequestTemplateReq req)
-            throws Exception {
-        QueryResponse<RequestTemplateResp> queryResponse = requestTemplateService.selectAllequestTemplateService(current, limit, req);
+    @PostMapping("/search/{page}/{pageSize}")
+    @ApiOperation(value = "search TaskTemplatePage ")
+    public JsonResponse<QueryResponse<TaskTemplateResp>> selectTaskTemplatePage(
+            @ApiParam(name = "page") @PathVariable("page") Integer page,
+            @ApiParam(name = "pageSize") @PathVariable("pageSize") Integer pageSize,
+            @RequestBody(required = false) SelectTaskTemplateRep rep
+    ) throws Exception {
+        QueryResponse<TaskTemplateResp> queryResponse = taskTemplateService.selectTaskTemplate(page, pageSize, rep);
+
         return JsonResponse.okayWithData(queryResponse);
     }
+
+    @GetMapping("/search")
+    @ApiOperation(value = "search TaskTemplateAll ")
+    public JsonResponse selectTaskTemplateAll() throws Exception {
+        List<TaskTemplateResp> taskTemplateRespList = taskTemplateService.selectTaskTemplateAll();
+        return JsonResponse.okayWithData(taskTemplateRespList);
+    }
+
 
     @GetMapping("/detail/{id}")
     @ApiOperation(value = "detail TaskTemplate", notes = "需要传入id")
     public JsonResponse detail(@PathVariable("id") String id) throws Exception {
-        requestTemplateService.deleteRequestTemplateService(id);
-        return JsonResponse.okay();
+        TaskTemplateResp templateResp= taskTemplateService.selectTaskTemplateOne(id);
+        return JsonResponse.okayWithData(templateResp);
     }
 }
 
--- src/main/java/com/webank/taskman/service/impl/TaskTemplateServiceImpl.java
+++ src/main/java/com/webank/taskman/service/impl/TaskTemplateServiceImpl.java
@@ -1,8 +1,17 @@
 package com.webank.taskman.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.webank.taskman.converter.TaskTemplateConverter;
+import com.webank.taskman.domain.RequestTemplateGroup;
 import com.webank.taskman.domain.TaskTemplate;
+import com.webank.taskman.dto.PageInfo;
+import com.webank.taskman.dto.QueryResponse;
 import com.webank.taskman.dto.req.SaveTaskTemplateReq;
+import com.webank.taskman.dto.req.SelectTaskTemplateRep;
+import com.webank.taskman.dto.resp.TaskTemplateResp;
 import com.webank.taskman.mapper.TaskTemplateMapper;
 import com.webank.taskman.service.TaskTemplateService;
 import org.apache.commons.lang3.StringUtils;
@@ -11,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 
 @Service
@@ -18,6 +28,9 @@ public class TaskTemplateServiceImpl extends ServiceImpl<TaskTemplateMapper, Tas
 
     @Autowired
     TaskTemplateMapper taskTemplateMapper;
+
+    @Autowired
+    TaskTemplateConverter taskTemplateConverter;
 
     @Override
     public TaskTemplate addOrUpdateTaskTemplate(SaveTaskTemplateReq taskTemplateReq) {
@@ -35,5 +48,61 @@ public class TaskTemplateServiceImpl extends ServiceImpl<TaskTemplateMapper, Tas
             return taskTemplateMapper.selectById(taskTemplate);
         }
         return null;
+    }
+
+    @Override
+    public void deleteTaskTemplateByIDService(String id) {
+        taskTemplateMapper.deleteTaskTemplateByIDMapper(id);
+    }
+
+    @Override
+    public QueryResponse<TaskTemplateResp> selectTaskTemplate(Integer currentPage, Integer pageSize, SelectTaskTemplateRep rep) {
+        Page<TaskTemplate> page = new Page<>(currentPage, pageSize);
+        QueryWrapper<TaskTemplate> wrapper = new QueryWrapper<>();
+        if (!StringUtils.isEmpty(rep.getId())) {
+            wrapper.eq("id", rep.getId());
+        }
+        if (!StringUtils.isEmpty(rep.getProcDefId())) {
+            wrapper.eq("proc_def_id", rep.getProcDefId());
+        }
+        if (!StringUtils.isEmpty(rep.getProcDefKey())) {
+            wrapper.eq("proc_def_key", rep.getProcDefKey());
+        }
+        if (!StringUtils.isEmpty(rep.getProcDefName())) {
+            wrapper.like("proc_def_name", rep.getProcDefName());
+        }
+        if (!StringUtils.isEmpty(rep.getProcNode())) {
+            wrapper.eq("proc_node", rep.getProcNode());
+        }
+        if (!StringUtils.isEmpty(rep.getName())) {
+            wrapper.like("name", rep.getName());
+        }
+        IPage<TaskTemplate> iPage = taskTemplateMapper.selectPage(page, wrapper);
+        List<TaskTemplate> records = iPage.getRecords();
+        List<TaskTemplateResp> taskTemplateResps = taskTemplateConverter.toDto(records);
+
+        QueryResponse<TaskTemplateResp> queryResponse = new QueryResponse<>();
+        PageInfo pageInfo = new PageInfo();
+        pageInfo.setStartIndex(iPage.getCurrent());
+        pageInfo.setPageSize(iPage.getSize());
+        pageInfo.setTotalRows(iPage.getTotal());
+        queryResponse.setPageInfo(pageInfo);
+        queryResponse.setContents(taskTemplateResps);
+        return queryResponse;
+    }
+
+    @Override
+    public List<TaskTemplateResp> selectTaskTemplateAll() {
+        List<TaskTemplate> taskTemplates = taskTemplateMapper.selectList(null);
+        List<TaskTemplateResp> taskTemplateResps = taskTemplateConverter.toDto(taskTemplates);
+        return taskTemplateResps;
+    }
+
+    @Override
+    public TaskTemplateResp selectTaskTemplateOne(String id) {
+        TaskTemplate taskTemplate = taskTemplateMapper.selectById(id);
+        TaskTemplateResp taskTemplateResp=new TaskTemplateResp();
+        BeanUtils.copyProperties(taskTemplate,taskTemplateResp);
+        return taskTemplateResp;
     }
 }

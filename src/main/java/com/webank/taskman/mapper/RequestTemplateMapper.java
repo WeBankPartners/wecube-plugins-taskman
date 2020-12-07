1efefe7cc6453b489f52a0d98c31e6259838b0bb
huangxiaobin-svg <hxb_sky@163.com>
2020-12-07 19:54:16 +0800

    #4 add custom Page select

--- src/main/java/com/webank/taskman/mapper/RequestTemplateMapper.java
+++ src/main/java/com/webank/taskman/mapper/RequestTemplateMapper.java
@@ -4,13 +4,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.webank.taskman.domain.RequestTemplate;
-import com.webank.taskman.dto.resp.RequestTemplateResp;
-
-import java.util.List;
+import com.webank.taskman.dto.req.QueryRequestTemplateReq;
 
 
 public interface RequestTemplateMapper extends BaseMapper<RequestTemplate> {
 
 
-    IPage<RequestTemplate> selectPageByParam(Page page, Object o);
+    IPage<RequestTemplate> selectPageByParam(Page page, QueryRequestTemplateReq queryReq);
 }

96c6a0d75fbc90886cafcbdac618e2470289d260
huangxiaobin-svg <hxb_sky@163.com>
2020-12-17 18:00:03 +0800

    #4 edit merge error: new_4_branch to dev

--- src/test/java/com/webank/taskman/TaskTest.java
+++ src/test/java/com/webank/taskman/TaskTest.java
@@ -1,11 +1,7 @@
 package com.webank.taskman;
 
-import com.webank.taskman.domain.Task;
-import com.webank.taskman.mapper.TaskMapper;
-import com.webank.taskman.service.TaskService;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 

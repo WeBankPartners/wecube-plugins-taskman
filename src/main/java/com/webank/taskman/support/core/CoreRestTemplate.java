8ffff7b3d5fdf1712d885db3f76ef3a5d1540804
huangxiaobin-svg <hxb_sky@163.com>
2020-12-18 17:25:15 +0800

    #4 edit inteface: URL workflow  platform

--- src/main/java/com/webank/taskman/support/core/CoreRestTemplate.java
+++ src/main/java/com/webank/taskman/support/core/CoreRestTemplate.java
@@ -22,9 +22,9 @@ public class CoreRestTemplate {
 
     @SuppressWarnings("unchecked")
     public <D, R extends CoreResponse> D get(String targetUrl, Class<R> responseType) throws CoreRemoteCallException {
-        log.info("V0.2.11 About to call {} ", targetUrl);
+        log.info("V0.2.21 About to call {} ", targetUrl);
         R jsonResponse = restTemplate.getForObject(targetUrl, responseType);
-        log.info("Core response: {} ", jsonResponse);
+        log.info("Core response: {} ", jsonResponse.getData());
         validateJsonResponse(jsonResponse);
         return (D) jsonResponse.getData();
     }

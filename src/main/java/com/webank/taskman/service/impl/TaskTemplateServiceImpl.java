46726e5e33386f112b9379c02ed99d6d875ce2ff
huangxiaobin-svg <hxb_sky@163.com>
2020-12-24 18:47:11 +0800

    #4 Remove invalid code

--- src/main/java/com/webank/taskman/service/impl/TaskTemplateServiceImpl.java
+++ src/main/java/com/webank/taskman/service/impl/TaskTemplateServiceImpl.java
@@ -1,8 +1,6 @@
 package com.webank.taskman.service.impl;
 
 
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -69,34 +67,6 @@ public class TaskTemplateServiceImpl extends ServiceImpl<TaskTemplateMapper, Tas
         TaskTemplateResp taskTemplateResp = new TaskTemplateResp();
         taskTemplateResp.setId(taskTemplateId);
         return taskTemplateResp;
-    }
-
-    @Override
-    public void deleteTaskTemplateByIDService(String id) {
-        UpdateWrapper<TaskTemplate> wrapper = new UpdateWrapper<>();
-        wrapper.eq("id", id).set("del_flag", 1);
-        taskTemplateMapper.update(null, wrapper);
-    }
-
-    @Override
-    public List<TaskTemplateResp> selectTaskTemplateAll() {
-        List<TaskTemplate> taskTemplates = taskTemplateMapper.selectList(null);
-        List<TaskTemplateResp> svResps = taskTemplateConverter.toDto(taskTemplates);
-        for (TaskTemplateResp svResp : svResps) {
-            List<RoleRelation> roles = roleRelationService.list(new QueryWrapper<RoleRelation>()
-                    .eq("record_id", svResp.getId()));
-            roles.stream().forEach(roleRelation -> {
-                RoleDTO roleDTO = new RoleDTO();
-                roleDTO.setRoleName(roleRelation.getRoleName());
-                roleDTO.setDisplayName(roleRelation.getRoleName());
-                if (RoleTypeEnum.USE_ROLE.getType() == roleRelation.getRoleType()) {
-                    svResp.getUseRoles().add(roleDTO);
-                } else {
-                    svResp.getManageRoles().add(roleDTO);
-                }
-            });
-        }
-        return svResps;
     }
 
     @Override

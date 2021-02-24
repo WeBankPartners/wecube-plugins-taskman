ab02f4aa38a8f7459ce0c0585719e6c938704c28
gavin2lee <gavin2lee@163.com>
2021-02-24 11:33:40 +0800

    #48 refactor service

--- src/main/java/com/webank/taskman/converter/FormItemInfoConverter.java
+++ src/main/java/com/webank/taskman/converter/FormItemInfoConverter.java
@@ -1,4 +1,5 @@
 package com.webank.taskman.converter;
+
 import com.webank.taskman.base.BaseConverter;
 import com.webank.taskman.domain.FormItemInfo;
 import com.webank.taskman.dto.CoreCreateTaskDTO.FormItemBean;
@@ -11,29 +12,32 @@ import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
 
 import java.util.List;
-@Mapper(componentModel = "spring",uses = {},unmappedTargetPolicy = ReportingPolicy.IGNORE)
+
+@Mapper(componentModel = "spring", uses = {}, unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface FormItemInfoConverter extends BaseConverter<FormItemInfoResp, FormItemInfo> {
 
     FormItemInfo toEntityByReq(SaveFormItemInfoReq req);
+
     List<FormItemInfo> toEntityByReq(List<SaveFormItemInfoReq> req);
 
-
-    @Mapping(target = "itemId",source = "id")
-    @Mapping(target = "key",source = "name")
-    @Mapping(target = "valueDef.type",source = "elementType")
-    @Mapping(target = "valueDef.expr",source = "value")
+    @Mapping(target = "itemId", source = "id")
+    @Mapping(target = "key", source = "name")
+    @Mapping(target = "valueDef.type", source = "elementType")
+    @Mapping(target = "valueDef.expr", source = "value")
     TaskServiceMetaFormItem respToServiceMeta(FormItemInfoResp resp);
+
     List<TaskServiceMetaFormItem> respToServiceMeta(List<FormItemInfoResp> resp);
 
-    @Mapping(target = "itemTempId",source = "itemId")
-    @Mapping(target = "name",source = "key")
-    @Mapping(target = "value",expression="java(bean.getVal().stream().collect(java.util.stream.Collectors.joining(\",\")))")
+    @Mapping(target = "itemTempId", source = "itemId")
+    @Mapping(target = "name", source = "key")
+    @Mapping(target = "value", expression = "java(bean.getVal().stream().collect(java.util.stream.Collectors.joining(\",\")))")
     FormItemInfo toEntityByBean(FormItemBean bean);
+
     List<FormItemInfo> toEntityByBean(List<FormItemBean> bean);
 
-    @Mapping( target = "value",expression ="java(attrValueDto.getDataValue()+\"\")")
+    @Mapping(target = "value", expression = "java(attrValueDto.getDataValue()+\"\")")
     FormItemInfo toEntityByAttrValue(EntityAttrValueDto attrValueDto);
-    List<FormItemInfo> toEntityByAttrValue(List<EntityAttrValueDto> attrValueDtos);
 
+    List<FormItemInfo> toEntityByAttrValue(List<EntityAttrValueDto> attrValueDtos);
 
 }

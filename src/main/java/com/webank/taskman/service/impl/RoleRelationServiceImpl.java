fe0684e9892cce4825a1c6a7b936f99c38041232
gavin2lee <gavin2lee@163.com>
2021-02-25 10:06:18 +0800

    #48 format java code

--- src/main/java/com/webank/taskman/service/impl/RoleRelationServiceImpl.java
+++ src/main/java/com/webank/taskman/service/impl/RoleRelationServiceImpl.java
@@ -1,6 +1,5 @@
 package com.webank.taskman.service.impl;
 
-
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.webank.taskman.constant.RoleTypeEnum;
 import com.webank.taskman.domain.RoleRelation;
@@ -14,29 +13,31 @@ import org.springframework.util.StringUtils;
 import java.util.List;
 
 @Service
-public class RoleRelationServiceImpl  extends ServiceImpl<RoleRelationMapper, RoleRelation> implements RoleRelationService {
-
+public class RoleRelationServiceImpl extends ServiceImpl<RoleRelationMapper, RoleRelation>
+        implements RoleRelationService {
 
     @Override
-    public int deleteByTemplate( String tempId) {
+    public int deleteByTemplate(String tempId) {
         return this.getBaseMapper().deleteByTemplate(tempId);
     }
 
     @Override
     public void saveRoleRelation(String recordId, RoleTypeEnum roleType, List<RoleDto> roles) {
-        roles.stream().forEach(role-> {
-            if(!StringUtils.isEmpty(role.getRoleName()) && !StringUtils.isEmpty(role.getDisplayName())  ){
-                this.getBaseMapper().insert( new RoleRelation(recordId, roleType.getType(),role.getRoleName(),role.getDisplayName()));
+        roles.stream().forEach(role -> {
+            if (!StringUtils.isEmpty(role.getRoleName()) && !StringUtils.isEmpty(role.getDisplayName())) {
+                this.getBaseMapper().insert(
+                        new RoleRelation(recordId, roleType.getType(), role.getRoleName(), role.getDisplayName()));
             }
         });
     }
 
     @Override
     @Transactional
-    public void saveRoleRelationByTemplate(String requestTemplateId, List<RoleDto> useRoles, List<RoleDto> manageRoles) {
+    public void saveRoleRelationByTemplate(String requestTemplateId, List<RoleDto> useRoles,
+            List<RoleDto> manageRoles) {
         deleteByTemplate(requestTemplateId);
-        saveRoleRelation(requestTemplateId, RoleTypeEnum.USE_ROLE,useRoles);
-        saveRoleRelation(requestTemplateId,RoleTypeEnum.MANAGE_ROLE,manageRoles);
+        saveRoleRelation(requestTemplateId, RoleTypeEnum.USE_ROLE, useRoles);
+        saveRoleRelation(requestTemplateId, RoleTypeEnum.MANAGE_ROLE, manageRoles);
     }
 
 }

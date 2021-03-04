aeb18dcf4982c8598eb3636c95af3f89840e50aa
gavin2lee <gavin2lee@163.com>
2021-03-04 10:29:43 +0800

    #48 add unit test

---
+++ taskman-core/src/test/java/com/webank/taskman/service/impl/FormTemplateServiceTest.java
@@ -0,0 +1,26 @@
+package com.webank.taskman.service.impl;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.webank.taskman.service.FormTemplateService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FormTemplateServiceTest {
+    
+    
+    @Autowired
+    FormTemplateService formTemplateService;
+
+    @Test
+    public void testSaveOrUpdateFormTemplate() {
+        //fail("Not yet implemented");
+    }
+
+}

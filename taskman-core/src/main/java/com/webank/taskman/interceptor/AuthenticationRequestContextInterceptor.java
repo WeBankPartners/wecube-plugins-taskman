10e2c63a53fa48e2223c8daaf5c82a468b4dbd40
gavin2lee <gavin2lee@163.com>
2021-03-19 16:00:37 +0800

    #48 refactor security interceptor

--- taskman-core/src/main/java/com/webank/taskman/interceptor/AuthenticationRequestContextInterceptor.java
+++ taskman-core/src/main/java/com/webank/taskman/interceptor/AuthenticationRequestContextInterceptor.java
@@ -1,41 +1,45 @@
 package com.webank.taskman.interceptor;
 
-import com.webank.taskman.commons.AuthenticationContextHolder;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.security.Principal;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import com.webank.taskman.commons.AuthenticationContextHolder;
+import com.webank.taskman.commons.AuthenticationContextHolder.AuthenticatedUser;
 
 @Component
 public class AuthenticationRequestContextInterceptor implements HandlerInterceptor {
-    private static final String AUTHORIZATION = "Authorization";
     public static final String REQ_ATTR_KEY_CURRENT_USER = "REQ_ATTR_KEY_CURRENT_USER";
 
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         Principal userPrincipal = request.getUserPrincipal();
-        if (userPrincipal != null) {
-            AuthenticationContextHolder.AuthenticatedUser currentUser = new AuthenticationContextHolder.AuthenticatedUser(userPrincipal.getName());
-
-            currentUser.withAuthorities(extractAuthorities(userPrincipal));
-            currentUser.setToken(request.getHeader(AUTHORIZATION));
+        if (userPrincipal != null && (userPrincipal instanceof Authentication)) {
+            Authentication auth = (Authentication)userPrincipal;
+            String authToken = (String) auth.getCredentials();
+            AuthenticatedUser currentUser = new AuthenticatedUser(auth.getName(),
+                    authToken, extractAuthorities(userPrincipal));
+            
             AuthenticationContextHolder.setAuthenticatedUser(currentUser);
 
             request.setAttribute(REQ_ATTR_KEY_CURRENT_USER, currentUser);
         }
-
         return true;
     }
 
-    private String[] extractAuthorities(Principal userPrincipal) {
-        String[] authorities = new String[0];
+    private List<String> extractAuthorities(Principal userPrincipal) {
+        List<String> authorities = new ArrayList<>();
         if (userPrincipal instanceof UsernamePasswordAuthenticationToken) {
-            authorities = ((UsernamePasswordAuthenticationToken) userPrincipal).getAuthorities().stream()
-                    .map(GrantedAuthority::toString).toArray(String[]::new);
+            ((UsernamePasswordAuthenticationToken) userPrincipal).getAuthorities().forEach(a -> {
+                authorities.add(a.getAuthority());
+            });
         }
         return authorities;
     }

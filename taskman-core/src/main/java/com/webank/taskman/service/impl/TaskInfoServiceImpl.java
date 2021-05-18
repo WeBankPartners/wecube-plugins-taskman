f3ebea70df529f19543ae3f7a794e64681507110
gavin2lee <gavin2lee@163.com>
2021-05-18 10:02:32 +0800

    #48 define dto to proceed user task.

--- taskman-core/src/main/java/com/webank/taskman/service/impl/TaskInfoServiceImpl.java
+++ taskman-core/src/main/java/com/webank/taskman/service/impl/TaskInfoServiceImpl.java
@@ -4,12 +4,16 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.webank.taskman.base.JsonResponse;
@@ -39,9 +43,14 @@ import com.webank.taskman.service.RequestInfoService;
 import com.webank.taskman.service.TaskInfoService;
 import com.webank.taskman.support.platform.PlatformCoreServiceRestClient;
 import com.webank.taskman.support.platform.dto.CommonPlatformResponseDto;
+import com.webank.taskman.support.platform.dto.PlatformPluginResponseDto;
+import com.webank.taskman.support.platform.dto.ProceedTaskResponseDto;
+import com.webank.taskman.support.platform.dto.TaskFormValueDto;
 
 @Service
 public class TaskInfoServiceImpl extends ServiceImpl<TaskInfoMapper, TaskInfo> implements TaskInfoService {
+
+    private static final Logger log = LoggerFactory.getLogger(TaskInfoServiceImpl.class);
 
     @Autowired
     private FormInfoConverter formInfoConverter;
@@ -66,19 +75,23 @@ public class TaskInfoServiceImpl extends ServiceImpl<TaskInfoMapper, TaskInfo> i
 
     @Autowired
     private PlatformCoreServiceRestClient platformServiceClient;
-    
+
     @Autowired
     private RequestInfoService requestInfoService;
 
+    private ObjectMapper objectMapper = new ObjectMapper();
+
     @Override
-    public LocalPageableQueryResult<TaskInfoDto> selectTaskInfo(Integer page, Integer pageSize, TaskInfoQueryReqDto req) {
+    public LocalPageableQueryResult<TaskInfoDto> selectTaskInfo(Integer page, Integer pageSize,
+            TaskInfoQueryReqDto req) {
         String inSql = req.getConditionSql();// req.getEqUseRole();
-        LambdaQueryWrapper<TaskInfo> queryWrapper = taskInfoConverter.convertToTaskInfoByQuery(req).getLambdaQueryWrapper()
-                .inSql(!StringUtils.isEmpty(inSql), TaskInfo::getId, inSql);
+        LambdaQueryWrapper<TaskInfo> queryWrapper = taskInfoConverter.convertToTaskInfoByQuery(req)
+                .getLambdaQueryWrapper().inSql(!StringUtils.isEmpty(inSql), TaskInfo::getId, inSql);
         PageHelper.startPage(page, pageSize);
-        PageInfo<TaskInfoDto> pages = new PageInfo<>(taskInfoConverter.convertToDtos(getBaseMapper().selectList(queryWrapper)));
-        LocalPageableQueryResult<TaskInfoDto> queryResponse = new LocalPageableQueryResult<>(pages.getTotal(), page.longValue(),
-                pageSize.longValue(), pages.getList());
+        PageInfo<TaskInfoDto> pages = new PageInfo<>(
+                taskInfoConverter.convertToDtos(getBaseMapper().selectList(queryWrapper)));
+        LocalPageableQueryResult<TaskInfoDto> queryResponse = new LocalPageableQueryResult<>(pages.getTotal(),
+                page.longValue(), pageSize.longValue(), pages.getList());
         return queryResponse;
     }
 
@@ -107,25 +120,61 @@ public class TaskInfoServiceImpl extends ServiceImpl<TaskInfoMapper, TaskInfo> i
         return JsonResponse.okay();
     }
 
+    private void asyncProceedUserTask() {
+        // TODO
+
+        String callbackUrl = null;
+        PlatformPluginResponseDto platformPluginResponseDto = null;
+        PlatformPluginResponseDto.ResultData resultData = null;
+
+        // TODO
+        ProceedTaskResponseDto proceedTaskRespDto = new ProceedTaskResponseDto();
+
+
+        TaskFormValueDto taskFormValueDto = buildTaskFormValueDto();
+        String taskFormOutput = convertTaskFormValueDtoToJson(taskFormValueDto);
+    }
+
+    private TaskFormValueDto buildTaskFormValueDto() {
+        // TODO
+        
+        return null;
+    }
+
+    private String convertTaskFormValueDtoToJson(TaskFormValueDto taskFormValueDto) {
+        try {
+            String json = objectMapper.writeValueAsString(taskFormValueDto);
+            return json;
+        } catch (JsonProcessingException e) {
+            log.error("Failed to write object to json.", e);
+            throw new TaskmanRuntimeException("Failed to write object to json.");
+        }
+    }
+
     private void callbackByTaskInfo(ProcessingTasksReqDto req, TaskInfo taskInfo) {
-//        CallbackRequestDto callbackRequest = new CallbackRequestDto();
-//        CallbackRequestResultDataDto callbackRequestResultDataDto = new CallbackRequestResultDataDto();
-//        callbackRequestResultDataDto.setRequestId(taskInfo.getRequestId());
-//        callbackRequestResultDataDto.setOutputs(
-//                Lists.newArrayList(new CallbackRequestOutputsDto(CallbackRequestOutputsDto.ERROR_CODE_SUCCESSFUL,
-//                        req.getResultMessage(), req.getResultMessage(), taskInfo.getCallbackParameter())));
-//
-//        callbackRequest.setResults(callbackRequestResultDataDto);
-//        callbackRequest.setResultMessage(req.getResultMessage());
-//        callbackRequest.setResultCode(req.getResult());
-//
-//        try {
-//            platformServiceClient.callback(taskInfo.getCallbackUrl(), callbackRequest);
-//        } catch (CoreRemoteCallException e) {
-//            String msg = String.format("Callback wecube meet error: %s", e.getMessage());
-//            log.error(msg);
-//            throw new TaskmanRuntimeException("3014", msg, e.getMessage());
-//        }
+        // CallbackRequestDto callbackRequest = new CallbackRequestDto();
+        // CallbackRequestResultDataDto callbackRequestResultDataDto = new
+        // CallbackRequestResultDataDto();
+        // callbackRequestResultDataDto.setRequestId(taskInfo.getRequestId());
+        // callbackRequestResultDataDto.setOutputs(
+        // Lists.newArrayList(new
+        // CallbackRequestOutputsDto(CallbackRequestOutputsDto.ERROR_CODE_SUCCESSFUL,
+        // req.getResultMessage(), req.getResultMessage(),
+        // taskInfo.getCallbackParameter())));
+        //
+        // callbackRequest.setResults(callbackRequestResultDataDto);
+        // callbackRequest.setResultMessage(req.getResultMessage());
+        // callbackRequest.setResultCode(req.getResult());
+        //
+        // try {
+        // platformServiceClient.callback(taskInfo.getCallbackUrl(),
+        // callbackRequest);
+        // } catch (CoreRemoteCallException e) {
+        // String msg = String.format("Callback wecube meet error: %s",
+        // e.getMessage());
+        // log.error(msg);
+        // throw new TaskmanRuntimeException("3014", msg, e.getMessage());
+        // }
     }
 
     @Override
@@ -146,35 +195,41 @@ public class TaskInfoServiceImpl extends ServiceImpl<TaskInfoMapper, TaskInfo> i
 
     @Override
     public TaskInfoRespDto taskInfoDetail(String id) {
-//        TaskInfo taskInfo = getBaseMapper().selectOne(new TaskInfo().setId(id).getLambdaQueryWrapper());
-//        TaskInfoRespDto taskInfoResp = taskInfoConverter.toResp(taskInfo);
-//        taskInfoResp.setFormItemInfo(formItemInfoService.returnDetail(id));
-//        return taskInfoResp;
-        
+        // TaskInfo taskInfo = getBaseMapper().selectOne(new
+        // TaskInfo().setId(id).getLambdaQueryWrapper());
+        // TaskInfoRespDto taskInfoResp = taskInfoConverter.toResp(taskInfo);
+        // taskInfoResp.setFormItemInfo(formItemInfoService.returnDetail(id));
+        // return taskInfoResp;
+
         return null;
     }
 
-
     @Override
     public RequestInfoInstanceResqDto selectTaskInfoInstanceService(String requestId, String taskId) {
-//        RequestInfo requestInfo = requestInfoService.getOne(new RequestInfo().setId(requestId).getLambdaQueryWrapper());
-//        RequestInfoInstanceResqDto requestInfoInstanceResq = requestInfoConverter.toInstanceResp(requestInfo);
-//
-//        requestInfoInstanceResq.setRequestFormResq(getFormInfoResq(requestId));
-//        List<TaskInfo> taskInfos = getBaseMapper().selectList(new QueryWrapper<TaskInfo>().lambda()
-//                .eq(TaskInfo::getRequestId, requestId).orderByAsc(TaskInfo::getUpdatedTime));
-//        List<TaskInfoInstanceRespDto> taskInfoInstanceResps = new ArrayList<>();
-//        for (TaskInfo taskInfo : taskInfos) {
-//            if (!(taskInfo.getId().equals(taskId))) {
-//                TaskInfoInstanceRespDto resp = taskInfoConverter.toInstanceResp(taskInfo);
-//                resp.setTaskFormResq(getFormInfoResq(taskId));
-//                taskInfoInstanceResps.add(resp);
-//            }
-//        }
-//        requestInfoInstanceResq.setTaskInfoInstanceResps(taskInfoInstanceResps);
-//
-//        return requestInfoInstanceResq;
-        
+        // RequestInfo requestInfo = requestInfoService.getOne(new
+        // RequestInfo().setId(requestId).getLambdaQueryWrapper());
+        // RequestInfoInstanceResqDto requestInfoInstanceResq =
+        // requestInfoConverter.toInstanceResp(requestInfo);
+        //
+        // requestInfoInstanceResq.setRequestFormResq(getFormInfoResq(requestId));
+        // List<TaskInfo> taskInfos = getBaseMapper().selectList(new
+        // QueryWrapper<TaskInfo>().lambda()
+        // .eq(TaskInfo::getRequestId,
+        // requestId).orderByAsc(TaskInfo::getUpdatedTime));
+        // List<TaskInfoInstanceRespDto> taskInfoInstanceResps = new
+        // ArrayList<>();
+        // for (TaskInfo taskInfo : taskInfos) {
+        // if (!(taskInfo.getId().equals(taskId))) {
+        // TaskInfoInstanceRespDto resp =
+        // taskInfoConverter.toInstanceResp(taskInfo);
+        // resp.setTaskFormResq(getFormInfoResq(taskId));
+        // taskInfoInstanceResps.add(resp);
+        // }
+        // }
+        // requestInfoInstanceResq.setTaskInfoInstanceResps(taskInfoInstanceResps);
+        //
+        // return requestInfoInstanceResq;
+
         return null;
     }
 
@@ -203,31 +258,33 @@ public class TaskInfoServiceImpl extends ServiceImpl<TaskInfoMapper, TaskInfo> i
         return taskInfoConverter.convertToDto(taskInfo);
     }
 
-//    @Override
-//    @Transactional
-//    public CommonResponseDto createTask(PlatformTaskCreationReqDto req) {
-//        if ( req.getInputs() == null || req.getInputs().isEmpty()) {
-//            throw new TaskmanRuntimeException(" inputs is null");
-//        }
-//        req.getInputs().stream().forEach(task -> {
-//            TaskInfo taskInfo = taskInfoConverter.toEntityByReq(task);
-//            /*
-//             * int isExists = taskInfoMapper.selectCount( new
-//             * TaskInfo(task.getProcInstId(),task.getTaskNodeId()).
-//             * getLambdaQueryWrapper()); if(0 < isExists){ throw new
-//             * TaskmanRuntimeException(String.format(
-//             * "Task is exists! procInstId:%s,TaskNodeId:%s",
-//             * task.getProcInstId(),task.getTaskNodeId())); }
-//             */
-//            taskInfo.setRequestId(req.getRequestId());
-//            // taskInfo.setCurrenUserName(taskInfo, taskInfo.getId());
-//            taskInfo.setCreatedBy(AuthenticationContextHolder.getCurrentUsername());
-//            taskInfo.setUpdatedBy(AuthenticationContextHolder.getCurrentUsername());
-//            save(taskInfo);
-//            List<FormItemInfo> items = formItemInfoConverter.toEntityByBean(task.getFormItems());
-//            formInfoService.saveFormInfoAndFormItems(items, taskInfo.getTaskTempId(), taskInfo.getId());
-//        });
-//        return CommonResponseDto.okay();
-//    }
+    // @Override
+    // @Transactional
+    // public CommonResponseDto createTask(PlatformTaskCreationReqDto req) {
+    // if ( req.getInputs() == null || req.getInputs().isEmpty()) {
+    // throw new TaskmanRuntimeException(" inputs is null");
+    // }
+    // req.getInputs().stream().forEach(task -> {
+    // TaskInfo taskInfo = taskInfoConverter.toEntityByReq(task);
+    // /*
+    // * int isExists = taskInfoMapper.selectCount( new
+    // * TaskInfo(task.getProcInstId(),task.getTaskNodeId()).
+    // * getLambdaQueryWrapper()); if(0 < isExists){ throw new
+    // * TaskmanRuntimeException(String.format(
+    // * "Task is exists! procInstId:%s,TaskNodeId:%s",
+    // * task.getProcInstId(),task.getTaskNodeId())); }
+    // */
+    // taskInfo.setRequestId(req.getRequestId());
+    // // taskInfo.setCurrenUserName(taskInfo, taskInfo.getId());
+    // taskInfo.setCreatedBy(AuthenticationContextHolder.getCurrentUsername());
+    // taskInfo.setUpdatedBy(AuthenticationContextHolder.getCurrentUsername());
+    // save(taskInfo);
+    // List<FormItemInfo> items =
+    // formItemInfoConverter.toEntityByBean(task.getFormItems());
+    // formInfoService.saveFormInfoAndFormItems(items, taskInfo.getTaskTempId(),
+    // taskInfo.getId());
+    // });
+    // return CommonResponseDto.okay();
+    // }
 
 }

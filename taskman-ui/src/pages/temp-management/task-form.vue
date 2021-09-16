b8f213d591c734e112d647301383d68426272999
pobu168 <244202969@qq.com>
2021-09-16 16:55:37 +0800

    form test

--- taskman-ui/src/pages/temp-management/task-form.vue
+++ taskman-ui/src/pages/temp-management/task-form.vue
@@ -1,17 +1,52 @@
 <template>
   <div class=" ">
-    task-form
+    <Tabs @on-click="changeNode" :value="currentNode">
+      <template v-for="node in nodes">
+        <TabPane :label="node.nodeName" :name="node.nodeId" :key="node.nodeId">
+          <FormComponent :ref="node.nodeId" :currentNode="currentNode" :node="node"></FormComponent>
+        </TabPane>
+      </template>
+    </Tabs>
   </div>
 </template>
 
 <script>
+import FormComponent from './task-form-component'
+import { getTemplateNodes } from '@/api/server.js'
 export default {
-  name: 'task-form',
+  name: '',
   data () {
-    return {}
+    return {
+      requestTemplateId: '614043ac9379fb1e',
+      currentNode: '',
+      nodes: []
+    }
   },
-  methods: {},
-  components: {}
+  mounted () {
+    this.getTemplateNodes()
+  },
+  methods: {
+    async getTemplateNodes () {
+      const { statusCode, data } = await getTemplateNodes(this.requestTemplateId)
+      if (statusCode === 'OK') {
+        this.nodes = data
+        this.currentNode = data[0].nodeId
+        // this.initTab(this.currentNode, data[0])
+        // this.$refs[this.currentNode].initData(data[0])
+      }
+    },
+    changeNode (nodeId) {
+      this.currentNode = nodeId
+      const find = this.nodes.find(n => n.nodeId === this.currentNode)
+      this.$refs[this.currentNode][0].initData(this.currentNode, find)
+    },
+    initTab (currentNode, data) {
+      this.$refs[this.currentNode][0].initData(data)
+    }
+  },
+  components: {
+    FormComponent
+  }
 }
 </script>
 

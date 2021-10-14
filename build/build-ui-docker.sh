c6ea11e7e5ec64027c776f2b3f729a524f661012
zgyzgyhero <894526647@qq.com>
2021-10-14 12:03:17 +0800

    Update build ui

--- build/build-ui-docker.sh
+++ build/build-ui-docker.sh
@@ -2,4 +2,4 @@
 set -e -x
 cd $(dirname $0)/../taskman-ui
 npm install
-npm run build
+npm run plugin
--- build/build-ui.sh
+++ build/build-ui.sh
@@ -5,7 +5,7 @@ if [ $? -eq 0 ]
 then
     cd $1/taskman-ui
     npm install
-    npm run build
+    npm run plugin
 else
     docker run --rm -v $1:/app/taskman --name wetaskman-node-build node:12.13.1 /bin/bash /app/taskman/build/build-ui-docker.sh
 fi

95a78e9212c369e438aeb65041619e400edb9b24
zgyzgyhero <894526647@qq.com>
2021-11-01 18:48:26 +0800

    Update config password

--- taskman-server/models/config.go
+++ taskman-server/models/config.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/WeBankPartners/go-common-lib/cipher"
 	"github.com/WeBankPartners/go-common-lib/token"
 	"io/ioutil"
@@ -79,11 +78,7 @@ func InitConfig(configFile string) (errMessage string) {
 		errMessage = "parse file to json fail," + err.Error()
 		return
 	}
-	rsaFileContent, err := ioutil.ReadFile(c.RsaKeyPath)
-	if err != nil {
-		errMessage = fmt.Sprintf("Read file %s fail,%s ", c.RsaKeyPath, err.Error())
-		return
-	}
+	rsaFileContent, _ := ioutil.ReadFile(c.RsaKeyPath)
 	c.Database.Password, err = cipher.DecryptRsa(c.Database.Password, string(rsaFileContent))
 	if err != nil {
 		errMessage = "init database password fail,%s " + err.Error()

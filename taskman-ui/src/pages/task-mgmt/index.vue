d87288973423a0b6fc83e917fa11028fde20b263
pobu168 <244202969@qq.com>
2023-11-01 09:51:31 +0800

    update: task commit control

--- taskman-ui/src/pages/task-mgmt/index.vue
+++ taskman-ui/src/pages/task-mgmt/index.vue
@@ -122,9 +122,15 @@
                       <Button v-if="data.editable" :disabled="enforceDisable" @click="saveTaskData" type="info">{{
                         $t('save')
                       }}</Button>
-                      <Button v-if="data.editable" :disabled="enforceDisable" @click="commitTaskData" type="primary">{{
-                        $t('commit')
-                      }}</Button>
+                      <Button
+                        v-if="data.editable"
+                        :disabled="
+                          enforceDisable || (data.nextOption && data.nextOption.length !== 0 && data.choseOption === '')
+                        "
+                        @click="commitTaskData"
+                        type="primary"
+                        >{{ $t('commit') }}</Button
+                      >
                     </div>
                   </div>
                 </span>

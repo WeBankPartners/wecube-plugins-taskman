4e601bf164dcfedf02f0852b6eb2d8492fd18bb2
prog-hjj <prog-hjj@foxmail.com>
2024-02-23 12:05:55 +0800

    任务模板增/改/列表接口

--- taskman-server/service/task_template_service.go
+++ taskman-server/service/task_template_service.go
@@ -1,12 +1,15 @@
 package service
 
 import (
+	"errors"
 	"fmt"
+	"strings"
+	"time"
+
 	"github.com/WeBankPartners/go-common-lib/guid"
 	"github.com/WeBankPartners/wecube-plugins-taskman/taskman-server/common/log"
 	"github.com/WeBankPartners/wecube-plugins-taskman/taskman-server/dao"
 	"github.com/WeBankPartners/wecube-plugins-taskman/taskman-server/models"
-	"time"
 )
 
 type TaskTemplateService struct {
@@ -248,11 +251,153 @@ func GetTaskTemplateMapByRequestTemplate(requestTemplate string) (taskTemplateMa
 }
 
 func (s TaskTemplateService) CreateCustomTaskTemplate(param *models.CustomTaskTemplateCreateParam) (*models.CustomTaskTemplateCreateResponse, error) {
-	result := &models.CustomTaskTemplateCreateResponse{}
+	actions := []*dao.ExecAction{}
+	// 查询现有任务模板列表
+	var taskTemplates []*models.TaskTemplateTable
+	err := s.taskTemplateDao.DB.SQL("SELECT * FROM task_template WHERE request_template = ? ORDER BY sort", param.RequestTemplate).Find(&taskTemplates)
+	if err != nil {
+		return nil, err
+	}
+	// 校验参数
+	if param.Sort <= 0 || param.Sort > len(taskTemplates)+1 {
+		return nil, errors.New("param sort out of range")
+	}
+	// 插入新任务模板
+	nowTime := time.Now().Format(models.DateTimeFormat)
+	newTaskTemplate := &models.TaskTemplateTable{
+		Id:              guid.CreateGuid(),
+		Type:            string(models.TaskTemplateTypeCustom),
+		Sort:            param.Sort,
+		RequestTemplate: param.RequestTemplate,
+		Name:            param.Name,
+		RoleType:        string(models.TaskTemplateRoleTypeCustom),
+		CreatedTime:     nowTime,
+		UpdatedTime:     nowTime,
+	}
+	action := &dao.ExecAction{Sql: "INSERT INTO task_template (id,type,sort,request_template,name,expire_day,role_type,created_time,updated_time) VALUES (?,?,?,?,?,?,?,?,?)"}
+	action.Param = []interface{}{newTaskTemplate.Id, newTaskTemplate.Type, newTaskTemplate.Sort, newTaskTemplate.RequestTemplate, newTaskTemplate.Name, newTaskTemplate.ExpireDay, newTaskTemplate.RoleType, newTaskTemplate.CreatedTime, newTaskTemplate.UpdatedTime}
+	actions = append(actions, action)
+	// 插入新任务处理模板
+	newTaskTemplateRole := &models.TaskTemplateRoleTable{
+		Id:             guid.CreateGuid(),
+		TaskTemplate:   newTaskTemplate.Id,
+		CustomRoleType: string(models.TaskTemplateRoleRoleTypeTemplate),
+		HandlerType:    string(models.TaskTemplateRoleHandlerTypeTemplate),
+	}
+	action = &dao.ExecAction{Sql: "INSERT INTO task_template_role (id,task_template,custom_role_type,handler_type) VALUES (?,?,?,?)"}
+	action.Param = []interface{}{newTaskTemplateRole.Id, newTaskTemplateRole.TaskTemplate, newTaskTemplateRole.CustomRoleType, newTaskTemplateRole.HandlerType}
+	actions = append(actions, action)
+	// 如果不是尾插，则需更新现有数据的序号
+	if param.Sort != len(taskTemplates)+1 {
+		for i := param.Sort; i < len(taskTemplates)+1; i++ {
+			t := taskTemplates[i-1]
+			t.Sort += 1
+			t.UpdatedTime = nowTime
+			action = &dao.ExecAction{Sql: "UPDATE task_template SET sort = ?, updated_time = ? WHERE id = ?"}
+			action.Param = []interface{}{t.Sort, t.UpdatedTime, t.Id}
+			actions = append(actions, action)
+		}
+		tmp := append(taskTemplates[:param.Sort-1], newTaskTemplate)
+		taskTemplates = append(tmp, taskTemplates[param.Sort-1:]...)
+	} else {
+		taskTemplates = append(taskTemplates, newTaskTemplate)
+	}
+	// 执行事务
+	err = dao.Transaction(actions)
+	if err != nil {
+		return nil, err
+	}
+	// 构造返回结果
+	result := &models.CustomTaskTemplateCreateResponse{
+		Id:              newTaskTemplate.Id,
+		Sort:            newTaskTemplate.Sort,
+		RequestTemplate: newTaskTemplate.RequestTemplate,
+		Name:            newTaskTemplate.Name,
+		Ids:             make([]*models.CustomTaskTemplateIdObj, len(taskTemplates)),
+	}
+	for i, taskTemplate := range taskTemplates {
+		result.Ids[i] = &models.CustomTaskTemplateIdObj{
+			Id:   taskTemplate.Id,
+			Sort: taskTemplate.Sort,
+			Name: taskTemplate.Name,
+		}
+	}
 	return result, nil
 }
 
 func (s TaskTemplateService) UpdateCustomTaskTemplate(param *models.TaskTemplateDto) error {
+	actions := []*dao.ExecAction{}
+	// 查询现有任务模板
+	var taskTemplates []*models.TaskTemplateTable
+	err := s.taskTemplateDao.DB.SQL("SELECT * FROM task_template WHERE id = ?", param.Id).Find(&taskTemplates)
+	if err != nil {
+		return err
+	}
+	if len(taskTemplates) == 0 {
+		return errors.New("no task_template record found")
+	}
+	taskTemplate := taskTemplates[0]
+	// 校验参数
+	if param.Type != string(models.TaskTemplateTypeCustom) {
+		return errors.New("param type wrong")
+	}
+	if taskTemplate.Type != param.Type || taskTemplate.Sort != param.Sort || taskTemplate.RequestTemplate != param.RequestTemplateId {
+		return errors.New("param type or sort or requestTemplate wrong")
+	}
+	for _, roleObj := range param.RoleObjs {
+		if roleObj.RoleType != string(models.TaskTemplateRoleRoleTypeTemplate) &&
+			(roleObj.HandlerType == string(models.TaskTemplateRoleHandlerTypeTemplate) || roleObj.HandlerType == string(models.TaskTemplateRoleHandlerTypeTemplateSuggest)) {
+			return fmt.Errorf("roleType %s not match handlerType %s", roleObj.RoleType, roleObj.HandlerType)
+		}
+		if roleObj.RoleType != string(models.TaskTemplateRoleRoleTypeTemplate) && roleObj.Role != "" {
+			return fmt.Errorf("roleType %s not match role %s", roleObj.RoleType, roleObj.Role)
+		}
+		if roleObj.HandlerType != string(models.TaskTemplateRoleHandlerTypeTemplate) && roleObj.HandlerType != string(models.TaskTemplateRoleHandlerTypeTemplateSuggest) && roleObj.Handler != "" {
+			return fmt.Errorf("handlerType %s not match handler %s", roleObj.HandlerType, roleObj.Handler)
+		}
+	}
+	// 更新现有任务模板
+	nowTime := time.Now().Format(models.DateTimeFormat)
+	action := &dao.ExecAction{Sql: "UPDATE task_template SET name = ?, expire_day = ?, description = ?, role_type = ?, updated_time = ? WHERE id = ?"}
+	action.Param = []interface{}{param.Name, param.ExpireDay, param.Description, param.RoleType, nowTime, param.Id}
+	actions = append(actions, action)
+	// 增删改现有任务处理模板
+	if param.RoleType == string(models.TaskTemplateRoleTypeAdmin) {
+		action = &dao.ExecAction{Sql: "DELETE FROM task_template_role WHERE task_template = ?"}
+		action.Param = []interface{}{param.Id}
+		actions = append(actions, action)
+	} else {
+		// 查询现有任务处理模板
+		var taskTemplateRoles []*models.TaskTemplateRoleTable
+		err = s.taskTemplateRoleDao.DB.SQL("SELECT * FROM task_template_role WHERE task_template = ?", param.Id).Find(&taskTemplateRoles)
+		if err != nil {
+			return err
+		}
+		// 对比增删改
+		for i, taskTemplateRole := range taskTemplateRoles {
+			if i < len(param.RoleObjs) {
+				roleObj := param.RoleObjs[i]
+				action = &dao.ExecAction{Sql: "UPDATE task_template_role SET custom_role_type = ?, handler_type = ?, custom_role = ?, handler = ? WHERE id = ?"}
+				action.Param = []interface{}{roleObj.RoleType, roleObj.HandlerType, roleObj.Role, roleObj.Handler, taskTemplateRole.Id}
+				actions = append(actions, action)
+			} else {
+				action = &dao.ExecAction{Sql: "DELETE FROM task_template_role WHERE id = ?"}
+				action.Param = []interface{}{taskTemplateRole.Id}
+				actions = append(actions, action)
+			}
+		}
+		for sort := len(taskTemplateRoles) + 1; sort <= len(param.RoleObjs); sort++ {
+			roleObj := param.RoleObjs[sort-1]
+			action = &dao.ExecAction{Sql: "INSERT INTO task_template_role (id,task_template,custom_role_type,handler_type,custom_role,handler) VALUES (?,?,?,?,?,?)"}
+			action.Param = []interface{}{guid.CreateGuid(), param.Id, roleObj.RoleType, roleObj.HandlerType, roleObj.Role, roleObj.Handler}
+			actions = append(actions, action)
+		}
+	}
+	// 执行事务
+	err = dao.Transaction(actions)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -273,5 +418,60 @@ func (s TaskTemplateService) ListCustomTaskTemplateIds(requestTemplateId string)
 
 func (s TaskTemplateService) ListCustomTaskTemplate(requestTemplateId string) ([]*models.TaskTemplateDto, error) {
 	result := []*models.TaskTemplateDto{}
+	// 查询现有任务模板列表
+	var taskTemplates []*models.TaskTemplateTable
+	err := s.taskTemplateDao.DB.SQL("SELECT * FROM task_template WHERE request_template = ? ORDER BY sort", requestTemplateId).Find(&taskTemplates)
+	if err != nil {
+		return nil, err
+	}
+	if len(taskTemplates) == 0 {
+		return result, nil
+	}
+	// 汇总任务模板列表
+	taskTemplateIds := make([]string, len(taskTemplates))
+	for i, taskTemplate := range taskTemplates {
+		taskTemplateIds[i] = taskTemplate.Id
+	}
+	// 查询现有任务处理模板列表
+	var taskTemplateRoles []*models.TaskTemplateRoleTable
+	err = s.taskTemplateRoleDao.DB.SQL("SELECT * FROM task_template_role WHERE task_template IN ('" + strings.Join(taskTemplateIds, "','") + "') ORDER BY task_template").Find(&taskTemplateRoles)
+	if err != nil {
+		return nil, err
+	}
+	// 汇总任务处理模板列表
+	taskTemplateRoleMap := make(map[string][]*models.TaskTemplateRoleTable)
+	taskTemplateId := ""
+	for _, taskTemplateRole := range taskTemplateRoles {
+		if taskTemplateId != taskTemplateRole.TaskTemplate {
+			taskTemplateId = taskTemplateRole.TaskTemplate
+			taskTemplateRoleMap[taskTemplateId] = make([]*models.TaskTemplateRoleTable, 0)
+		}
+		taskTemplateRoleMap[taskTemplateId] = append(taskTemplateRoleMap[taskTemplateId], taskTemplateRole)
+	}
+	// 构造返回结果
+	result = make([]*models.TaskTemplateDto, len(taskTemplates))
+	for i, taskTemplate := range taskTemplates {
+		result[i] = &models.TaskTemplateDto{
+			Id:                taskTemplate.Id,
+			Type:              taskTemplate.Type,
+			Sort:              taskTemplate.Sort,
+			RequestTemplateId: taskTemplate.RequestTemplate,
+			Name:              taskTemplate.Name,
+			ExpireDay:         taskTemplate.ExpireDay,
+			Description:       taskTemplate.Description,
+			RoleType:          taskTemplate.RoleType,
+		}
+		if roleObjs, ok := taskTemplateRoleMap[taskTemplate.Id]; ok {
+			result[i].RoleObjs = make([]*models.TaskTemplateRoleDto, len(roleObjs))
+			for j, roleObj := range roleObjs {
+				result[i].RoleObjs[j] = &models.TaskTemplateRoleDto{
+					RoleType:    roleObj.CustomRoleType,
+					HandlerType: roleObj.HandlerType,
+					Role:        roleObj.CustomRole,
+					Handler:     roleObj.Handler,
+				}
+			}
+		}
+	}
 	return result, nil
 }

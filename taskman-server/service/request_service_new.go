636f6d51243d8253fbb7fefefc3d6e1f7edd6565
tangjiawei <tangjiawei@nnyy.com>
2024-03-10 21:12:55 +0800

    请求确认bug修复

--- taskman-server/service/request_service_new.go
+++ taskman-server/service/request_service_new.go
@@ -1105,7 +1105,7 @@ func RequestConfirm(param models.RequestConfirmParam, user string) error {
 	// 更新处理节点
 	actions = append(actions, &dao.ExecAction{Sql: "update task_handle set handle_result = ?,updated_time = ?,handle_status = ? where task = ?", Param: []interface{}{models.TaskHandleResultTypeApprove, now, models.TaskHandleResultTypeComplete, param.TaskId}})
 	// 更新请求确认任务设置为已完成
-	actions = append(actions, &dao.ExecAction{Sql: "update task set status = ?,tak_result = ?,description = ? ,updated_by = ?,updated_time = ? where id = ?", Param: []interface{}{models.TaskStatusDone, models.TaskResultTypeComplete, param.Notes, user, now, param.TaskId}})
+	actions = append(actions, &dao.ExecAction{Sql: "update task set status = ?,task_result = ?,description = ? ,updated_by = ?,updated_time = ? where id = ?", Param: []interface{}{models.TaskStatusDone, models.TaskResultTypeComplete, param.Notes, user, now, param.TaskId}})
 	// 更新请求表状态,设置为完成
 	actions = append(actions, &dao.ExecAction{Sql: "update request set status = ?,complete_status = ?,updated_by = ?,updated_time= ? where id = ?", Param: []interface{}{models.RequestStatusCompleted, param.CompleteStatus, user, now, param.Id}})
 	return dao.Transaction(actions)

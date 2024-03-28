fa44f16c563117f64254d5db09077ea54583ef65
prog-hjj <prog-hjj@foxmail.com>
2024-03-28 20:08:02 +0800

    任务模板校验检查逻辑修改

--- taskman-server/service/task_template_service.go
+++ taskman-server/service/task_template_service.go
@@ -146,20 +146,20 @@ func (s *TaskTemplateService) CheckHandleTemplates(param *models.TaskTemplateDto
 				return fmt.Errorf("param assign %s not match handlerType %s", handleTemplate.Assign, handleTemplate.HandlerType)
 			}
 			if handleTemplate.Assign == string(models.TaskHandleTemplateAssignTypeTemplate) {
-				if handleTemplate.Role != "" {
+				if handleTemplate.Role == "" {
 					return fmt.Errorf("param assign %s not match role %s", handleTemplate.Assign, handleTemplate.Role)
 				}
 			} else {
-				if handleTemplate.Role == "" {
+				if handleTemplate.Role != "" {
 					return fmt.Errorf("param assign %s not match role %s", handleTemplate.Assign, handleTemplate.Role)
 				}
 			}
 			if handleTemplate.HandlerType == string(models.TaskHandleTemplateHandlerTypeTemplate) || handleTemplate.HandlerType == string(models.TaskHandleTemplateHandlerTypeTemplateSuggest) {
-				if handleTemplate.Handler != "" {
+				if handleTemplate.Handler == "" {
 					return fmt.Errorf("param handlerType %s not match handler %s", handleTemplate.HandlerType, handleTemplate.Handler)
 				}
 			} else {
-				if handleTemplate.Handler == "" {
+				if handleTemplate.Handler != "" {
 					return fmt.Errorf("param handlerType %s not match handler %s", handleTemplate.HandlerType, handleTemplate.Handler)
 				}
 			}

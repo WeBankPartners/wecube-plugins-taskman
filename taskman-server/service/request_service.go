91f741ac49fd9572a9d37ab6b5a6342ff8436b12
tangjiawei <tangjiawei@nnyy.com>
2024-06-07 15:46:40 +0800

    处理历史优化

--- taskman-server/service/request_service.go
+++ taskman-server/service/request_service.go
@@ -2277,7 +2277,8 @@ func getTaskFormData(taskObj *models.TaskForHistory) (result []*models.RequestPr
 		for _, formTmpl := range formTemplates {
 			if formTmpl.ItemGroupType == string(models.FormItemGroupTypeWorkflow) {
 				actualFormTemplateIds = append(actualFormTemplateIds, formTmpl.Id)
-			} else {
+			}
+			if formTmpl.RefId != "" {
 				actualFormTemplateIds = append(actualFormTemplateIds, formTmpl.RefId)
 			}
 		}

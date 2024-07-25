b1f30397e5eb31eb57b1ca9811084ec28d570f36
Alexaa123 <1239327906@qq.com>
2024-07-25 11:10:20 +0800

    解决taskman的i18n的问题3

--- taskman-ui/src/main-plugin.js
+++ taskman-ui/src/main-plugin.js
@@ -9,17 +9,17 @@ import enUS from '@/locale/i18n/en-US.json'
 import { ValidationProvider } from 'vee-validate'
 // import './vee-validate-local-config'
 
-import Dashboard from '@/pages/workbench/index.vue'
+// import Dashboard from '@/pages/workbench/index.vue'
 
 window.component('ValidationProvider', ValidationProvider)
-window.addHomepageComponent &&
-  window.addHomepageComponent({
-    code: 'TASK_WORKBENCH',
-    name: () => {
-      return window.vm.$t('tw_workbench')
-    },
-    component: Dashboard
-  })
+// window.addHomepageComponent &&
+//   window.addHomepageComponent({
+//     code: 'TASK_WORKBENCH',
+//     name: () => {
+//       return window.vm.$t('tw_workbench')
+//     },
+//     component: Dashboard
+//   })
 
 window.locale('zh-CN', zhCN)
 window.locale('en-US', enUS)

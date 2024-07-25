5b1178ee689d6702ab42ae09078dca9c5cf15c99
Alexaa123 <1239327906@qq.com>
2024-07-25 16:52:33 +0800

    解决taskman的i18n的问题15

--- taskman-ui/src/router-plugin.js
+++ taskman-ui/src/router-plugin.js
@@ -6,6 +6,25 @@ import requestManagementIndex from '@/pages/request-management/index.vue'
 import task from '@/pages/task.vue'
 import taskManagementIndex from '@/pages/task-mgmt/index.vue'
 import requestCheck from '@/pages/request-management/request-check.vue'
+import workbenchIndex from '@/pages/workbench/index.vue'
+import workbenchTemplate  from '@/pages/workbench/template/index.vue'
+import workbenchPublishCreate from '@/pages/workbench/publish/create'
+import workbenchPublishDetail from '@/pages/workbench/publish/detail'
+import workbenchPublishList  from '@/pages/workbench/publish/list'
+import workbenchRequestCreate from '@/pages/workbench/request/create'
+import workbenchRequestDetail from '@/pages/workbench/request/detail'
+import workbenchRequestList from '@/pages/workbench/request/list'
+import workbenchProblemCreate from '@/pages/workbench/problem/create'
+import workbenchProblemDetail from '@/pages/workbench/problem/detail'
+import workbenchProblemList from '@/pages/workbench/problem/list'
+import workbenchEventCreate from '@/pages/workbench/event/create'
+import workbenchEventDetail from '@/pages/workbench/event/detail'
+import workbenchEventList from '@/pages/workbench/event/list'
+import workbenchChangeCreate from '@/pages/workbench/change/create'
+import workbenchChangeDetail from '@/pages/workbench/change/detail'
+import workbenchChangeList from '@/pages/workbench/change/list'
+import workbenchRequestAudit from '@/pages/workbench/request-audit.vue'
+
 const router = [
   {
     path: '/taskman/template-mgmt',
@@ -57,103 +76,103 @@ const router = [
       {
         path: '/taskman/workbench/dashboard',
         name: '/taskman/workbench/dashboard',
-        component: () => import('@/pages/workbench/index.vue')
+        component: workbenchIndex
       },
       // 模板选择
       {
         path: '/taskman/workbench/template',
         name: '/taskman/workbench/template',
-        component: () => import('@/pages/workbench/template/index')
+        component: workbenchTemplate
       },
       // 新建发布
       {
         path: '/taskman/workbench/createPublish',
         name: '/taskman/workbench/createPublish',
-        component: () => import('@/pages/workbench/publish/create')
+        component: workbenchPublishCreate
       },
       // 发布详情
       {
         path: '/taskman/workbench/detailPublish',
         name: '/taskman/workbench/detailPublish',
-        component: () => import('@/pages/workbench/publish/detail')
+        component: workbenchPublishDetail
       },
       // 发布历史
       {
         path: '/taskman/workbench/publishHistory',
         name: '/taskman/workbench/publishHistory',
-        component: () => import('@/pages/workbench/publish/list')
+        component: workbenchPublishList
       },
       // 新建请求
       {
         path: '/taskman/workbench/createRequest',
         name: '/taskman/workbench/createRequest',
-        component: () => import('@/pages/workbench/request/create')
+        component: workbenchRequestCreate
       },
       // 请求详情
       {
         path: '/taskman/workbench/detailRequest',
         name: '/taskman/workbench/detailRequest',
-        component: () => import('@/pages/workbench/request/detail')
+        component: workbenchRequestDetail
       },
       // 请求历史
       {
         path: '/taskman/workbench/requestHistory',
         name: '/taskman/workbench/requestHistory',
-        component: () => import('@/pages/workbench/request/list')
+        component: workbenchRequestList
       },
       // 新建问题
       {
         path: '/taskman/workbench/createProblem',
         name: '/taskman/workbench/createProblem',
-        component: () => import('@/pages/workbench/problem/create')
+        component: workbenchProblemCreate
       },
       // 问题详情
       {
         path: '/taskman/workbench/detailProblem',
         name: '/taskman/workbench/detailProblem',
-        component: () => import('@/pages/workbench/problem/detail')
+        component: workbenchProblemDetail
       },
       // 问题历史
       {
         path: '/taskman/workbench/problemHistory',
         name: '/taskman/workbench/problemHistory',
-        component: () => import('@/pages/workbench/problem/list')
+        component: workbenchProblemList
       },
       // 新建事件
       {
         path: '/taskman/workbench/createEvent',
         name: '/taskman/workbench/createEvent',
-        component: () => import('@/pages/workbench/event/create')
+        component: workbenchEventCreate
       },
       // 事件详情
       {
         path: '/taskman/workbench/detailEvent',
         name: '/taskman/workbench/detailEvent',
-        component: () => import('@/pages/workbench/event/detail')
+        component: workbenchEventDetail
       },
       // 事件历史
       {
         path: '/taskman/workbench/eventHistory',
         name: '/taskman/workbench/eventHistory',
-        component: () => import('@/pages/workbench/event/list')
+        component: workbenchEventList
       },
       // 新建变更
       {
         path: '/taskman/workbench/createChange',
         name: '/taskman/workbench/createChange',
-        component: () => import('@/pages/workbench/change/create')
+        component: workbenchChangeCreate
       },
       // 变更详情
       {
         path: '/taskman/workbench/detailChange',
         name: '/taskman/workbench/detailChange',
-        component: () => import('@/pages/workbench/change/detail')
+        component: workbenchChangeDetail
       },
       // 变更历史
       {
         path: '/taskman/workbench/changeHistory',
         name: '/taskman/workbench/changeHistory',
-        component: () => import('@/pages/workbench/change/list')
+        component: workbenchChangeList
       }
     ]
   },
@@ -161,7 +180,7 @@ const router = [
   {
     path: '/taskman/requestAudit',
     name: '/taskman/requestAudit',
-    component: () => import('@/pages/workbench/request-audit.vue')
+    component: workbenchRequestAudit
   }
 ]
 export default router

1458b03ffc475333843aef22c705c79a8afe6188
tangjiawei <tangjiawei@nnyy.com>
2024-10-11 16:50:12 +0800

    #114 Add workflow execution association request

--- taskman-server/dao/request_dao.go
+++ taskman-server/dao/request_dao.go
@@ -10,6 +10,6 @@ type RequestDao struct {
 }
 
 func (d *RequestDao) QueryRequestByProcInstanceId(procInstanceId string) (result []*models.RequestTable, err error) {
-	err = d.DB.SQL("select id,name,request_template from request where proc_instance_id = ?", procInstanceId).Find(&result)
+	err = d.DB.SQL("select id,name,request_template,status,created_by,created_time,updated_by,updated_time,type from request where proc_instance_id = ?", procInstanceId).Find(&result)
 	return
 }

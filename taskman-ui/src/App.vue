28f4ca199e6087cbdddbbebf21d301eab74883ce
何琦 <993284758@qq.com>
2025-01-21 20:28:15 +0800

    cover iview back top

--- taskman-ui/src/App.vue
+++ taskman-ui/src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div id="app">
     <div class="app-content-container">
-      <BackTop :height="100" :bottom="100" />
+      <BackTop :height="100" :bottom="100">
+        <div class="w-back-top">
+          <img :src="fesUpIcon" style="width: 24px;"/>
+
+        </div>
+      </BackTop>
       <router-view />
     </div>
   </div>
 </template>
 <script>
+import fesUpIcon from '@/images/icon/fes_up.svg'
 export default {
+  data () {
+    return {
+      fesUpIcon
+    }
+  },
   methods: {
     jump (path) {
       this.$router.push({ path: path })
@@ -26,6 +37,20 @@ export default {
 
   .app-content-container {
     height: 100%;
+
+    .w-back-top {
+      width: 44px;
+      height: 44px;
+      background-color: #fff;
+      border-radius: 22px;
+      box-shadow: 0 2px 8px 0 rgba(15, 18, 34, 0.1);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      &:hover {
+        background-color: #f5f8ff;
+      }
+    }
   }
 }
 #nav {

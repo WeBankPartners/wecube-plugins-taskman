11fbfbe39fcb3b594577f82a9d974bca6e9ac43b
何琦 <993284758@qq.com>
2025-01-23 15:18:19 +0800

    remove iview css in main-plugin

--- taskman-ui/src/main-plugin.js
+++ taskman-ui/src/main-plugin.js
@@ -1,7 +1,7 @@
 import 'regenerator-runtime/runtime'
 import router from './router-plugin'
 import routerP from './router-plugin-p'
-import 'view-design/dist/styles/iview.css'
+
 // import './locale/i18n'
 import zhCN from '@/locale/i18n/zh-CN.json'
 import enUS from '@/locale/i18n/en-US.json'

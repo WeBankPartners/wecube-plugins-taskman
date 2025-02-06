3bd96eb45bac17767b179e03906e5a86fa03dd77
wanghao7717 <792974788@qq.com>
2025-02-06 14:36:03 +0800

    feat: npm构建脚本修改

--- build/build-ui.sh
+++ build/build-ui.sh
@@ -4,7 +4,7 @@ npm -v
 if [ $? -eq 0 ]
 then
     cd $1/taskman-ui
-    npm install
+    npm install --legacy-peer-deps
     npm run plugin
 else
     docker run --rm -v $1:/app/taskman --name wetaskman-node-build node:12.13.1 /bin/bash /app/taskman/build/build-ui-docker.sh

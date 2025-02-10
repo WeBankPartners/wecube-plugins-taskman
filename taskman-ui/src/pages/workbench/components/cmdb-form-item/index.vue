15b1d255b404989f39fecdbf1555a8adef5ed323
wanghao7717 <792974788@qq.com>
2025-02-10 11:12:24 +0800

    feat: 密码字段兼容列权限控制

--- taskman-ui/src/pages/workbench/components/cmdb-form-item/index.vue
+++ taskman-ui/src/pages/workbench/components/cmdb-form-item/index.vue
@@ -2,7 +2,7 @@
  * @Author: wanghao7717 792974788@qq.com
  * @Date: 2024-10-18 17:55:45
  * @LastEditors: wanghao7717 792974788@qq.com
- * @LastEditTime: 2025-02-07 19:59:55
+ * @LastEditTime: 2025-02-10 10:34:39
 -->
 <template>
   <div class="cmdb-entity-table">
@@ -12,6 +12,7 @@
         :panalData="value"
         :allSensitiveData="allSensitiveData"
         :rowData="rowData"
+        :type="type"
         :disabled="isGroupEditDisabled(column, value)"
         @input="(v) => {value[column.inputKey] = v}"
       />

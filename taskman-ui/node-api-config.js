dbbdb62d5676c361840f3441076eef4919d83794
wanghao7717 <792974788@qq.com>
2025-03-03 20:31:01 +0800

    feat: 项目启动命令修改

--- taskman-ui/node-api-config.js
+++ taskman-ui/node-api-config.js
@@ -5,7 +5,7 @@
  * @Author: wanghao7717 792974788@qq.com
  * @Date: 2025-02-21 11:05:22
  * @LastEditors: wanghao7717 792974788@qq.com
- * @LastEditTime: 2025-02-27 16:41:52
+ * @LastEditTime: 2025-03-03 17:45:35
  *
  * 备注：采用该脚本生成API权限JSON，需注意以下几点：
  * 1. 页面中用到的API必须在server.js中定义
@@ -29,21 +29,21 @@ const WRITE_API_PATH = path.resolve(path.resolve('..'), 'taskman-server/conf') /
 // -------------------------------------------------生成组件对应api的引用关系--------------------------------------------------------------
 
 // 根据import语句返回对应API函数列表
-function extractApiImports(content) {
+function extractApiImports (content) {
   // 匹配 import { *** } from '***server(.js)?' 这种形式的导入语句
-  const importRegex = /import\s+\{([^}]+)\}\s+from\s+['"](?:[^'"]*?)server(\.js)?['"]/g;
-  let match;
-  const apiLists = [];
+  const importRegex = /import\s+\{([^}]+)\}\s+from\s+['"](?:[^'"]*?)server(\.js)?['"]/g
+  let match
+  const apiLists = []
   // 使用全局匹配，循环提取所有匹配的导入语句
   while ((match = importRegex.exec(content))) {
     const apiList = match[1]
       .trim()
       .split(',')
-      .map(item => item.trim());
-    apiLists.push(apiList);
+      .map(item => item.trim())
+    apiLists.push(apiList)
   }
   // 将所有 API 列表合并为一个数组
-  return apiLists.flat();
+  return apiLists.flat()
 }
 
 // 提取 custom_api_enum 内容
--- taskman-ui/vue.config.js
+++ taskman-ui/vue.config.js
@@ -1,7 +1,7 @@
 const CompressionPlugin = require('compression-webpack-plugin')
 const postcssWrap = require('postcss-wrap')
 const path = require('path')
-const baseUrl = process.env.VUE_APP_API || 'http://127.0.0.1/'
+const baseUrl = process.env.BASE_URL
 module.exports = {
   devServer: {
     // hot: true,

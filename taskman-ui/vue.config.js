218a5216078896d543ba5adba8cb23203183179e
wanghao7717 <792974788@qq.com>
2025-03-10 11:54:30 +0800

    feat: 修改entry publicPath参数

--- taskman-ui/vue.config.js
+++ taskman-ui/vue.config.js
@@ -22,7 +22,7 @@ module.exports = {
     }
   },
   runtimeCompiler: true,
-  publicPath: '/',
+  publicPath: '/ui-resources/taskman/v1.4.1.63/plugin/',
   chainWebpack: config => {
     if (process.env.PLUGIN !== 'plugin') {
       // remove the old loader
